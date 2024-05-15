a18db0c2affefe3cbf440272a536f6b0bdf3ccdc
sathish1980 <kumar.sathish189@gmail.com>
2024-05-15 08:58:09 +0530

    threads

---
+++ src/main/java/ThreadsConcept/SyncronizedThread.java
@@ -0,0 +1,52 @@
+package ThreadsConcept; 
+
+public class SyncronizedThread {
+	
+	synchronized void printTable(int a, int total) throws InterruptedException
+	{
+		for(int i=1;i<total;i++)
+		{
+			System.out.println(a +" * "+i +" = " +a * i);
+		}
+	}
+	
+	
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		Thread t = new Thread()
+		{
+	SyncronizedThread s = new SyncronizedThread();
+	public void run()
+	{
+		try {
+			s.printTable(3, 10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+		};
+t.start();
+t.join();
+
+Thread t1 = new Thread()
+{
+SyncronizedThread s = new SyncronizedThread();
+public void run()
+{
+try {
+	s.printTable(5, 10);
+} catch (InterruptedException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+}
+};
+t1.start();
+
+}
+
+}
